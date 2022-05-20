8c647311174adf756044a2cd2b7e5bba96af4c0b
Michael <michaelfarquhar4@gmail.com>
2022-05-19 20:25:05 -0400

    Improve validation for registration form

--- src/validation/messages.ts
+++ src/validation/messages.ts
@@ -6,6 +6,13 @@ export const tooShort = (input: string) => {
     return `${input} is too short.`;
 };
 
+export const tooLong = (input: string) => {
+    return `${input} is too long.`;
+};
+
 export const badEmail = () => {
     return `Please enter a valid email.`;
+};
+export const noSpaces = (input: string) => {
+    return `${input} cannot contain spaces.`;
 };
